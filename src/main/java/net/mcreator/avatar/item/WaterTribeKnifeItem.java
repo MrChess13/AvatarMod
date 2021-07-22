29e85d501ec11a171fbfee3cfd093af93efcd635
MrChess13 <Dolev@DolevPC>
2021-07-22 17:53:57 +0300

    Added More recipe for ice blocks

---
+++ src/main/java/net/mcreator/avatar/item/WaterTribeKnifeItem.java
@@ -0,0 +1,51 @@
+
+package net.mcreator.avatar.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.item.crafting.Ingredient;
+import net.minecraft.item.SwordItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.IItemTier;
+
+import net.mcreator.avatar.itemgroup.AvatarModTabItemGroup;
+import net.mcreator.avatar.AvatarModElements;
+
+@AvatarModElements.ModElement.Tag
+public class WaterTribeKnifeItem extends AvatarModElements.ModElement {
+	@ObjectHolder("avatar:water_tribe_knife")
+	public static final Item block = null;
+	public WaterTribeKnifeItem(AvatarModElements instance) {
+		super(instance, 104);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new SwordItem(new IItemTier() {
+			public int getMaxUses() {
+				return 400;
+			}
+
+			public float getEfficiency() {
+				return 4f;
+			}
+
+			public float getAttackDamage() {
+				return 6f;
+			}
+
+			public int getHarvestLevel() {
+				return 1;
+			}
+
+			public int getEnchantability() {
+				return 2;
+			}
+
+			public Ingredient getRepairMaterial() {
+				return Ingredient.EMPTY;
+			}
+		}, 3, -3f, new Item.Properties().group(AvatarModTabItemGroup.tab)) {
+		}.setRegistryName("water_tribe_knife"));
+	}
+}
